a7baab3b8ea224aded5228a83aa9dedca3933b96
Oscar D. Corbalan <oscardc@gmx.com>
2015-03-10 10:20:18 +0100

    Add pág Hechoal.com

---
+++ pages/hecho-al-punto.php
@@ -0,0 +1,24 @@
+<?php include '../header.php'; ?> 
+<section id="portfolio-elem">
+	<div class="graficos">
+		<p><img src="/img/hecho-al-punto/screen.jpg" height="1054"></p>			
+	</div>
+	<div class="resumen">
+		<header>
+			<h1>Hecho al Punto</h1>
+			<p>Web</p>
+		</header>
+		<section id="portfolio-descr">
+			<p>
+				Hecho al Punto es mi marca personal y portal de referencia para mis servicios como profesional freelance.
+			</p>
+			<p>Enlaces:
+				<ul>
+					<li><a href="https://github.com/OscarDoc/Html5-HechoAlPunto" target="blank">Código en GitHub</a></li>
+				</ul>
+			</p>
+			<a class="portfolio-url" href="http://www.hechoal.com" target="blank">hechoal.com</a>
+		</section>
+	</div>		
+</section>
+<?php include '../footer.php'; ?>
