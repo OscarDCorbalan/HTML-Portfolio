f02f3402894f1253a0ee2d18ecde1a15c530941f
Oscar D. Corbalan <oscardc@gmx.com>
2015-03-10 13:42:55 +0100

    Index reorder

--- index.php
+++ index.php
@@ -24,14 +24,14 @@ include 'header.php';
 	echo (article('dungeon-realms','Dungeon Realms','Juego','jpg'));
 	echo (article('chromatic-td','Chromatic TD','Juego','png'));
 	echo (article('biwair','Biwair','Juego','jpg'));
+	echo (article('mancala','Mancala','Juego','jpg'));
+	echo (article('sinistra-defense','Sinistra Defense','Juego HTML5','png'));
 	echo (article('tierra-media','Tierra Media','Mod para NWN','jpg'));
 	echo (article('bitmaiden','Bit Maiden','Web / Wordpress','jpg'));
 	echo (article('hecho-al-punto','Hecho al Punto','Web','png'));
 	echo (article('estanco','Estanco de Llagostera','Web','jpg'));
-	echo (article('taxi-san-roque','Taxi San Roque','Web','png'));
-	echo (article('mancala','Mancala','Juego','jpg'));	
-	echo (article('roxbury','Roxbury: The Club','Web','jpg'));
-	echo (article('sinistra-defense','Sinistra Defense','Juego HTML5','png'));
+	echo (article('taxi-san-roque','Taxi San Roque','Web','png'));		
+	echo (article('roxbury','Roxbury: The Club','Web','jpg'));		
 	?>
 </section>
 <?php include 'footer.php'; ?>
