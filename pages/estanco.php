833f8bd8e017ba1bbf93006533b350f0bc98b514
Oscar D. Corbalan <oscardc@gmx.com>
2015-03-10 10:41:38 +0100

    Add pág Estanco

---
+++ pages/estanco.php
@@ -0,0 +1,20 @@
+<?php include '../header.php'; ?> 
+<section id="portfolio-elem">
+	<div class="graficos">
+		<p><img src="/img/estanco/screen1.jpg" height="390"></p>			
+		<p><img src="/img/estanco/screen2.jpg" height="350"></p>			
+	</div>
+	<div class="resumen">
+		<header>
+			<h1>Estanco de Llagostera</h1>
+			<p>Web</p>
+		</header>
+		<section id="portfolio-descr">
+			<p>
+				Web creada para el Estanco de Llagostera, junto a tarjetas de visita y aplicación de la LOPD.
+			</p>
+			<a class="portfolio-url" href="http://www.estancollagostera.com" target="blank">estancollagostera.com</a>
+		</section>
+	</div>		
+</section>
+<?php include '../footer.php'; ?>
