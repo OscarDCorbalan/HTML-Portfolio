ee3266c8fdc16497ced2a5f9d6885137ffa3f187
Oscar D. Corbalan <oscardc@gmx.com>
2015-03-10 16:11:00 +0100

    Add footer

--- footer.php
+++ footer.php
@@ -1,7 +1,17 @@
 			</div> <?php //cierra class="pagewidth" ?>
-			<footer>
-				
-			</footer>
 		</div> <?php //cierra id="main" ?>
+		<footer>
+			<div class="pagewidth">
+			<section id="about">
+				<h1>Sobre mi</h1>
+				<p>Soy un desarrollador barcelonés que disfruta con los retos. Soy un políglota de la programación: no lo sé todo de ningún lenguaje, pero escribo en multitud de ellos y aprendo nuevos fácilmente.</p>
+				<p>Amante de la guitarra y de la mecánica DIY.</p>
+			</section>
+			<section id="about">
+				<h1>Contacto</h1>
+				<p>oscardc@gmx.com</p>
+			</section>
+			</div>
+		</footer>	
 	</body>
 </html>
--- index.php
+++ index.php
@@ -19,19 +19,33 @@ HTML;
 include 'header.php';
 ?>
 <section id="portfolio">
-	<?php
-	echo (article('geoparsing','Geoparsing','Desarrollo Java','jpg'));
-	echo (article('dungeon-realms','Dungeon Realms','Juego','jpg'));
-	echo (article('chromatic-td','Chromatic TD','Juego','png'));
-	echo (article('biwair','Biwair','Juego','jpg'));
-	echo (article('mancala','Mancala','Juego','jpg'));
-	echo (article('sinistra-defense','Sinistra Defense','Juego HTML5','png'));
-	echo (article('tierra-media','Tierra Media','Mod para NWN','jpg'));
-	echo (article('bitmaiden','Bit Maiden','Web / Wordpress','jpg'));
-	echo (article('hecho-al-punto','Hecho al Punto','Web','png'));
-	echo (article('estanco','Estanco de Llagostera','Web','jpg'));
-	echo (article('taxi-san-roque','Taxi San Roque','Web','png'));		
-	echo (article('roxbury','Roxbury: The Club','Web','jpg'));		
-	?>
+	<h2 id="portfolio-title">Proyectos</h2>
+	<div>
+		<?php
+		echo (article('geoparsing','Geoparsing','Desarrollo Java','jpg'));
+		echo (article('dungeon-realms','Dungeon Realms','Juego','jpg'));
+		echo (article('chromatic-td','Chromatic TD','Juego','png'));
+		echo (article('biwair','Biwair','Juego','jpg'));
+		echo (article('mancala','Mancala','Juego','jpg'));
+		echo (article('sinistra-defense','Sinistra Defense','Juego HTML5','png'));
+		echo (article('tierra-media','Tierra Media','Mod para NWN','jpg'));
+		echo (article('bitmaiden','Bit Maiden','Web / Wordpress','jpg'));
+		echo (article('hecho-al-punto','Hecho al Punto','Web','png'));
+		echo (article('estanco','Estanco de Llagostera','Web','jpg'));
+		echo (article('taxi-san-roque','Taxi San Roque','Web','png'));		
+		echo (article('roxbury','Roxbury: The Club','Web','jpg'));		
+		?>
+	</div>
+</section>
+<section id="trabajos">
+	<h2>Otros trabajos</h2>
+	<ul>
+		<li>Detección de movimiento con Raspberry PI para cámaras de locales.</li>
+		<li>Plugin Wordpress para crear landings personalizados para cada producto en venta.</li>
+		<li>Aplicación web de gestión de vehículos de cortesía para taller de automoción.</li>
+		<li>Implantaciones LOPD (Ley Ogánica de Protección de Datos).</li>		
+		<li>Reparaciones gama blanca y gama marrón.</li>
+		<li>Formador en ofimática.</li>
+	</ul>
 </section>
 <?php include 'footer.php'; ?>
