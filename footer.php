0b16e735d2925c87532cd804d2a664d32bafaeb5
Oscar D. Corbalan <oscardc@gmx.com>
2015-03-10 16:40:30 +0100

    Add formulario en footer

--- footer.php
+++ footer.php
@@ -3,13 +3,42 @@
 		<footer>
 			<div class="pagewidth">
 			<section id="about">
-				<h1>Sobre mi</h1>
+				<h2>Sobre mi</h2>
 				<p>Soy un desarrollador barcelonés que disfruta con los retos. Soy un políglota de la programación: no lo sé todo de ningún lenguaje, pero escribo en multitud de ellos y aprendo nuevos fácilmente.</p>
 				<p>Amante de la guitarra y de la mecánica DIY.</p>
 			</section>
 			<section id="about">
-				<h1>Contacto</h1>
-				<p>oscardc@gmx.com</p>
+				<form id="form1" name="form1" class="wufoo topLabel page" accept-charset="UTF-8" autocomplete="off" enctype="multipart/form-data" method="post" novalidate
+				action="https://oscardc.wufoo.com.mx/forms/ztt9y4c179vhcz/#public">	  
+					<header id="header" class="info">
+						<h2>Contacto</h2>
+					</header>
+					<ul>
+						<li id="foli1" class="notranslate">
+							<label class="desc" id="title1" for="Field1">Correo electrónico</label>
+							<div>
+								<input id="Field1" name="Field1" type="email" spellcheck="false" class="field text medium" value="" maxlength="255" tabindex="1"       />
+							</div>
+						</li>
+						<li id="foli2" class="notranslate">
+							<label class="desc" id="title2" for="Field2">Tu mensaje</label>
+							<div>
+								<textarea id="Field2" name="Field2" class="field textarea medium" spellcheck="true" rows="10" cols="50" tabindex="2" onkeyup=""></textarea>
+							</div>
+						</li>
+						<li class="buttons ">
+							<div>
+								<input id="saveForm" name="saveForm" class="btTxt submit" type="submit" value="Enviar"/>
+							</div>
+						</li>
+						<li class="hide">
+							<label for="comment">Do Not Fill This Out</label>
+							<textarea name="comment" id="comment" rows="1" cols="1"></textarea>
+							<input type="hidden" id="idstamp" name="idstamp" value="dBrgARN+gtkxAkb0jwEI8zjqCV1NiKLRrUqhi831sXc=" />
+						</li>
+					</ul>
+				</form>
+				<p class="peque">También puedes enviarme un correo a oscardc@gmx.com.</p>
 			</section>
 			</div>
 		</footer>	
