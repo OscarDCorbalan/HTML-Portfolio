fb8a0b4adeda5ec7c7707cb7ddd2a4fa50574568
Oscar D. Corbalan <oscardc@gmx.com>
2015-03-10 10:41:50 +0100

    Add pág Taxi San Roque

---
+++ pages/taxi-san-roque.php
@@ -0,0 +1,19 @@
+<?php include '../header.php'; ?> 
+<section id="portfolio-elem">
+	<div class="graficos">
+		<p><img src="/img/taxi-san-roque/screen.jpg" height="445"></p>	
+	</div>
+	<div class="resumen">
+		<header>
+			<h1>Taxi San Roque</h1>
+			<p>Web</p>
+		</header>
+		<section id="portfolio-descr">
+			<p>
+				Web creada para un taxista de la localidad turística de San Roque, en búsqueda de captar clientes mediante posicionamiento SEO.
+			</p>
+			<a class="portfolio-url" href="http://www.taxisanroque.com" target="blank">taxisanroque.com</a>
+		</section>
+	</div>		
+</section>
+<?php include '../footer.php'; ?>
