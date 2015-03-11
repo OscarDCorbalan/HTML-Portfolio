b91152685a5130fd81e8d10d20b23b35dc82a58c
Oscar D. Corbalan <oscardc@gmx.com>
2015-03-11 09:48:35 +0100

    Add astro en hobbies

--- hobbies.php
+++ hobbies.php
@@ -2,8 +2,9 @@
 $currentPage = 'hobby';
 include 'header.php';
 ?>
-<section id="musica">
-	<h2>Musica</h2>
+<div id="hobbies">
+	<section id="musica">
+		<h2>Musica</h2>
 		<p>He estudiado guitarra y música durante varios años. Si tienes un navegador moderno (soporta HTML5) puedes escuchar estas piezas tocadas por mi.</p>
 		<p>La primera pieza es Bourrée en mi menor, de Bach (s.XVIII), una obra originalmente compuesta para laúd.</p>
 		<audio controls> <!-- Si no soporta MP4, carga OGG y, si no, output del <p> -->
@@ -23,5 +24,12 @@ include 'header.php';
 			<source src="/audio/Oscar_Improvis.oga" type="audio/ogg;">
 			<p>Tu navegador no soporta el elemento 'audio' de HTML5.</p>
 		</audio>
-</section>
+	</section>
+	<section id="astro">
+		<h2>Astro*</h2>
+		<p>Me gustan las ciencias como la astrofísica y la astrobiología. He traducido artículos con el grupo AstroSeti, llegando a recibir dos Certificados de Reconocimiento, del Instituto de Astrobiología de la NASA y del Science@NASA.</p>
+		<img src="/img/nasa/nai.jpg" alt="Nasa Astrobiology Institute">
+		<img src="/img/nasa/science.jpg" alt="Nasa Astrobiology Institute">
+	</section>
+</div>
 <?php include 'footer.php'; ?>
