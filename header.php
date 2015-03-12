b5e5cfc2c20156f1e982d26e8d94cd7e446fa260
Oscar D. Corbalan <oscardc@gmx.com>
2015-03-12 13:30:23 +0100

    IE fix

--- header.php
+++ header.php
@@ -14,8 +14,8 @@
 				</a>			
 				<nav>
 					<a href="/" class="<?php if($currentPage == 'index'): ?>active<?php endif; ?>">Proyectos</a>
-					<a href="/files/CV_Oscar_Dominguez.pdf" >CV (PDF)</a>
 					<a href="http://github.com/OscarDoc" target="_blank">GitHub</a>
+					<a href="/files/CV_Oscar_Dominguez.pdf" >CV (PDF)</a>					
 					<a href="/hobbies.php" class="<?php if($currentPage == 'hobby'): ?>active<?php endif; ?>">Hobbies</a>
 					<a href="#contacto" id="contact">Contacto</a>
 				</nav>      
